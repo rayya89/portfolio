33a145f532f0e7449bf413e30f1730d8b1bb6d0f
Rayya <rayyatar@gmail.com>
2022-05-11 21:35:13 +0200

    last polish

--- src/Components/ModalCard.jsx
+++ src/Components/ModalCard.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import closeIcon from "../Assets/xmark.svg";
-import "../Styles/ModalCard.css"
+import "../Styles/ModalCard.css";
 
 export default function ModalCard({ setModal, projectInfo }) {
-  const { id, screenshot, title, description, techUsed, webLink, gitLink } = projectInfo;
+  const { id, screenshot, title, description, techUsed, webLink, gitLink } =
+    projectInfo;
 
   return (
-        <div className="modal-card">
-          <div className="img-modal">
-            <img className="screenshot" src={screenshot} alt="project screenshot"/>
-            <img onClick={() => setModal(null)} className="modal-icon" src={closeIcon} alt="close icon" />
-          </div>
-              <h3>{title}</h3>
-              <p>{description}</p>
-              <div className="modal-tech">
-              {techUsed.map((techUsed) => {
-                return <span key={id}>{techUsed}</span>;
-              })}
-              </div>
-              <a href={webLink}>
-              <button className="button-primary">Visit website</button>
-              </a>
-              <a href={gitLink}>
-              <button className="button-secondary">Git repository</button>
-              </a>
-        </div>
+    <div className="modal-card">
+      <div className="img-modal">
+        <img className="screenshot" src={screenshot} alt="project screenshot" />
+        <img
+          onClick={() => setModal(null)}
+          className="modal-icon"
+          src={closeIcon}
+          alt="close icon"
+        />
+      </div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <div className="modal-tech">
+        {techUsed.map((techUsed) => {
+          return <span key={id}>{techUsed}</span>;
+        })}
+      </div>
+      <a href={webLink} target="_blank" rel="noreferrer noopener">
+        <button className="button-primary">Visit website</button>
+      </a>
+      <a href={gitLink} target="_blank" rel="noreferrer noopener">
+        <button className="button-secondary">Git repository</button>
+      </a>
+    </div>
   );
 }
